671e0ef13f4cf49e077169a4d05ac861636d6887
Nikki Fernandez <nikkif@sourcepad.com>
2011-09-12 02:28:38 -0400

    Initial Commit

---
+++ ProSniperTrainer/ProSniperTrainerAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ProSniperTrainerAppDelegate.h
+//  ProSniperTrainer
+//
+//  Created by Nikki Fernandez on 9/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProSniperTrainerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ ProSniperTrainer/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ProSniperTrainer
+//
+//  Created by Nikki Fernandez on 9/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
